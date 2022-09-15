c48847541db9de1fdd658191cb644ff2d2cd4304
Emilly Maia <emillypereira1290@gmail.com>
2022-09-15 09:28:26 -0300

    Initial Commit

---
+++ SmelliciousTest/SmelliciousTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmelliciousTestApp.swift
+//  SmelliciousTest
+//
+//  Created by Emilly Maia on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SmelliciousTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
