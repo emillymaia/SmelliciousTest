ccdc5260013bbcc37b2df1a53c1ead8d7a541c8b
Helena <helenaoliveira@alu.ufc.br>
2022-09-19 10:33:45 -0300

    adjusting smokeColor

--- SmelliciousTest/Model/EssenceModel.swift
+++ SmelliciousTest/Model/EssenceModel.swift
@@ -14,6 +14,7 @@ struct EssenceModel: Identifiable, Hashable, Equatable {
     var id = UUID().uuidString
     var value: String
     var icon: String
+    var smokeColor: String
     var padding: CGFloat = 10
     var textSize: CGFloat = .zero
     var fontSize: CGFloat = 19
@@ -28,15 +29,15 @@ struct EssenceModel: Identifiable, Hashable, Equatable {
 
 var essences_: [EssenceModel] = [
     EssenceModel(value: "Hortelã", icon: "Hortela", smokeColor: "Verde"),
-    EssenceModel(value: "Maracujá", icon: "Maracuja", smokeColor: "Amarelo" ),
+    EssenceModel(value: "Maracujá", icon: "Maracuja", smokeColor: "Amarelo"),
     EssenceModel(value: "Rosas", icon: "Rosas", smokeColor: "Vermelho"),
-    EssenceModel(value: "Tangerina", icon: "Tangerina", smokeColor: "Laranja"),
-    EssenceModel(value: "Limão", icon: "Limao", smokeColor: "Verde"),
-    EssenceModel(value: "Canela", icon: "Canela", smokeColor: "Marrom"),
-    EssenceModel(value: "Baunilha", icon: "Baunilha", smokeColor: "Creme"),
+    EssenceModel(value: "Tangerina", icon: "Tangerina", smokeColor:"Laranja"),
+    EssenceModel(value: "Limão", icon: "Limao", smokeColor:"Verde"),
+    EssenceModel(value: "Canela", icon: "Canela", smokeColor:"Marrom"),
+    EssenceModel(value: "Baunilha", icon: "Baunilha", smokeColor:"Creme"),
     EssenceModel(value: "Lavanda", icon: "Lavanda", smokeColor: "Roxo"),
-    EssenceModel(value: "Ylang Ylang", icon: "Ylang", smokeColor: "Amarelo"),
-    EssenceModel(value: "Algodão", icon: "Algodao", smokeColor: "Creme"),
+    EssenceModel(value: "Ylang Ylang", icon: "Ylang", smokeColor:"Amarelo"),
+    EssenceModel(value: "Algodão", icon: "Algodao", smokeColor:"Creme"),
     EssenceModel(value: "Flor de Cerejeira", icon: "Cerejeira", smokeColor: "Rosa"),
-    EssenceModel(value: "Coco", icon: "Coco", smokeColor: "Marrom")
+    EssenceModel(value: "Coco", icon: "Coco", smokeColor:"Marrom")
 ]
