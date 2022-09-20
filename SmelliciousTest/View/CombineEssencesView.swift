796c3f7ac3e381435ae04a31228d8fcd4c2f5e6f
Calisto <mateus.bruno.calisto@gmail.com>
2022-09-20 11:52:36 -0300

    reset func end

--- SmelliciousTest/View/CombineEssencesView.swift
+++ SmelliciousTest/View/CombineEssencesView.swift
@@ -13,28 +13,22 @@ struct CombineEssencesView: View {
     
     @State var essence1: EssenceModel? = nil
     @State var essence2: EssenceModel? = nil
-    @State var isShowingPopover = false
+    @State var popoverPositive = false
+    @State var popoverNegative = false
     @State var smokeName = "defaultSmoke"
-    
-    
     
     func ResetButton() -> some View {
         Button {
-            resetEssence()
+            smokeName = "defaultSmoke"
+            essence1 = nil
+            essence2 = nil
         }label: {
             Image(systemName: "arrow.clockwise")
                 .resizable()
         }
         .foregroundColor(Color.init( red: 0.19, green: 0.28, blue: 0.23))
-        
     }
     
-    func resetEssence() {
-        smokeName = "defaultSmoke"
-        essence1 = nil
-        essence2 = nil
-        
-    }
     
     
     var body: some View {
@@ -53,25 +47,20 @@ struct CombineEssencesView: View {
                             
                             DropArea(essence: essence1){ id in
                                 let droppedEssence = essences.first { essence in
-                                    
                                     return essence.id == id
                                 }
-                                
                                 essence1 = droppedEssence
                                 checkMisture()
                                 smokeName = (essence1?.smokeColor)!
-                                
                             }
                             
                             DropArea2(essence: essence2) { id in
                                 let droppedEssence = essences.first { essence in
                                     return essence.id == id
                                 }
-                                
                                 essence2 = droppedEssence
                                 checkMisture()
                                 smokeName = (essence2?.smokeColor)!
-                                
                             }
                         }
                         
@@ -91,50 +80,9 @@ struct CombineEssencesView: View {
             }
         }
         .overlay {
-            if isShowingPopover {
-                VisualEffectView(effect: UIBlurEffect(style: .light))
-                    .ignoresSafeArea(.all)
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color(red: 0.89, green: 0.95, blue: 0.91))
-                    .frame(width: 400, height: 200)
-                    .overlay {
-                        Text("YAAAAY!")
-                            .foregroundColor(Color(red: 0.20, green: 0.29, blue: 0.24))
-                            .font(Font.title.weight(.bold))
-                            .font(.system(size: 25))
-                            .offset(x: 5, y: -65)
-                        Text("Sua combinação é um sucesso! Relaxe e aproveite seu novo aroma! Que tal ler um livro para deixar esse momento perfeito?")
-                            .foregroundColor(Color(red: 0.20, green: 0.29, blue: 0.24))
-                            .font(.system(size: 16))
-                            .frame(width: 250, height: 100)
-                            .offset(x: 65, y: -10)
-                        Image("Baunilha")
-                            .frame(width: 100 , height: 100, alignment: .center)
-                            .clipShape(Circle())
-                            .shadow(radius: 10)
-                            .offset(x: -120.0, y: -25)
-                        Button(action: {
-                            withAnimation {
-                                isShowingPopover = false
-                            }
-                            resetEssence()
-                        }) {
-                            Text("Dimiss")
-                                .frame(width: 400 , height: 50, alignment: .center)
-                                .background(Color(.white))
-                                .foregroundColor(.black)
-                                .clipShape(Rectangle())
-                        }
-                        
-                        .offset(x: 0.0, y: 75.0)
-                    }
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .transition(.scale)
-                
-            }
+            SwiftUIView(popoverPositive: $popoverPositive, popoverNegative: $popoverNegative, smokeName: $smokeName, essence1: $essence1, essence2: $essence2)
             
-        }
-        
+       }
     }
     
     
@@ -185,19 +133,24 @@ struct CombineEssencesView: View {
             return
         }
         
+        
         if essence2.niceMistures.contains(essence1.value) {
-            
             withAnimation {
-                isShowingPopover = true
+                popoverPositive = true
             }
             
         } else {
-            print("deu errado")
+            withAnimation {
+                popoverNegative = true
+            }
         }
+        
+        
+        
+        
+        
     }
 }
-
-
 
 struct CombineEssencesView_Previews: PreviewProvider {
     static var previews: some View {
--- SmelliciousTest/View/SwiftUIView.swift
+++ SmelliciousTest/View/SwiftUIView.swift
@@ -8,67 +8,114 @@
 import SwiftUI
 
 struct SwiftUIView: View {
-    
-    @State var isShowingPopover = false
+    @Binding var popoverPositive: Bool
+    @Binding var popoverNegative: Bool
+    @Binding var smokeName: String
+    @Binding var essence1: EssenceModel?
+    @Binding var essence2: EssenceModel?
     
     var body: some View {
-        ZStack {
-            Color(UIColor.systemIndigo)
-            Button("Show") {
-                withAnimation {
-                    isShowingPopover = true
-                }
-            }
-        }
-        .overlay {
-            if isShowingPopover {
+        
+        if popoverNegative {
             VisualEffectView(effect: UIBlurEffect(style: .light))
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color(red: 0.89, green: 0.95, blue: 0.91))
-                    .frame(width: 400, height: 200)
-                    .overlay {
-                        Text("YAAAAY!")
-                            .foregroundColor(Color(red: 0.20, green: 0.29, blue: 0.24))
-                            .font(Font.title.weight(.bold))
-                            .font(.system(size: 25))
-                            .offset(x: 5, y: -65)
-                        Text("Sua combinação é um sucesso! Relaxe e aproveite seu novo aroma! Que tal ler um livro para deixar esse momento perfeito?")
-                            .foregroundColor(Color(red: 0.20, green: 0.29, blue: 0.24))
-                            .font(.system(size: 16))
-                            .frame(width: 250, height: 100)
-                            .offset(x: 65, y: -10)
-                        Image("Baunilha")
-                           .frame(width: 100 , height: 100, alignment: .center)
-                           .clipShape(Circle())
-                           .shadow(radius: 10)
-                           .offset(x: -120.0, y: -25)
-                        Button(action: {
-                            withAnimation {
-                                isShowingPopover = false
-                            }
-                        }) {
-                            Text("Dimiss")
-                                .frame(width: 400 , height: 50, alignment: .center)
-                                .background(Color(.white))
-                                .foregroundColor(.black)
-                                .clipShape(Rectangle())
+                .edgesIgnoringSafeArea(.all)
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color(red: 0.89, green: 0.95, blue: 0.91))
+                .frame(width: 400, height: 200)
+                .overlay {
+                    Text("AFFFFF!")
+                        .foregroundColor(Color(red: 0.20, green: 0.29, blue: 0.24))
+                        .font(Font.title.weight(.bold))
+                        .font(.system(size: 25))
+                        .offset(x: 5, y: -65)
+                    Text("Sua combinação é um sucesso! Relaxe e aproveite seu novo aroma! Que tal ler um livro para deixar esse momento perfeito?")
+                        .foregroundColor(Color(red: 0.20, green: 0.29, blue: 0.24))
+                        .font(.system(size: 16))
+                        .frame(width: 250, height: 100)
+                        .offset(x: 65, y: -10)
+                    Image("Baunilha")
+                        .frame(width: 100 , height: 100, alignment: .center)
+                        .clipShape(Circle())
+                        .shadow(radius: 10)
+                        .offset(x: -120.0, y: -25)
+                    Button(action: {
+                        resetEssence()
+                        withAnimation {
+                            popoverNegative = false
                         }
-                        .offset(x: 0.0, y: 75.0)
+                        
+                    }) {
+                        Text("Dimiss")
+                            .frame(width: 400 , height: 50, alignment: .center)
+                            .background(Color(.white))
+                            .foregroundColor(.black)
+                            .clipShape(Rectangle())
                     }
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .transition(.scale)
-            }
+                    .offset(x: 0.0, y: 75.0)
+                }
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .transition(.scale)
         }
+        
+        
+        if popoverPositive {
+            VisualEffectView(effect: UIBlurEffect(style: .light))
+                .edgesIgnoringSafeArea(.all)
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color(red: 0.89, green: 0.95, blue: 0.91))
+                .frame(width: 400, height: 200)
+                .overlay {
+                    Text("YAAAAY!")
+                        .foregroundColor(Color(red: 0.20, green: 0.29, blue: 0.24))
+                        .font(Font.title.weight(.bold))
+                        .font(.system(size: 25))
+                        .offset(x: 5, y: -65)
+                    Text("Sua combinação é um sucesso! Relaxe e aproveite seu novo aroma! Que tal ler um livro para deixar esse momento perfeito?")
+                        .foregroundColor(Color(red: 0.20, green: 0.29, blue: 0.24))
+                        .font(.system(size: 16))
+                        .frame(width: 250, height: 100)
+                        .offset(x: 65, y: -10)
+                    Image("Baunilha")
+                        .frame(width: 100 , height: 100, alignment: .center)
+                        .clipShape(Circle())
+                        .shadow(radius: 10)
+                        .offset(x: -120.0, y: -25)
+                    Button(action: {
+                        resetEssence()
+                        withAnimation {
+                            popoverPositive = false
+                        }
+                    }) {
+                        Text("Dimiss")
+                            .frame(width: 400 , height: 50, alignment: .center)
+                            .background(Color(.white))
+                            .foregroundColor(.black)
+                            .clipShape(Rectangle())
+                    }
+                    .offset(x: 0.0, y: 75.0)
+                }
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .transition(.scale)
+        }
+        
+    }
+    func resetEssence() {
+        smokeName = "defaultSmoke"
+        essence1 = nil
+        essence2 = nil
     }
 }
+
+
+
 struct VisualEffectView: UIViewRepresentable {
     var effect: UIVisualEffect?
     func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
     func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
 }
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
-    }
-}
+//struct SwiftUIView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SwiftUIView(popoverPositive: .constant(true), popoverNegative: .constant(true))
+//    }
+//}
 
