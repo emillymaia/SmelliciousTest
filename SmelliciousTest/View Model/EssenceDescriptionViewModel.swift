6bb56b3940033059d29a039737143be0accc19ea
Narely Lima <92168493+NarelyLima@users.noreply.github.com>
2022-10-24 10:31:47 -0300

    Update EssenceDescriptionViewModel.swift
    Import UIKit to use their properties

--- SmelliciousTest/View Model/EssenceDescriptionViewModel.swift
+++ SmelliciousTest/View Model/EssenceDescriptionViewModel.swift
@@ -3,16 +3,15 @@
 //  SmelliciousTest
 //
 //  Created by Emilly Maia on 27/09/22.
-//
 
 import SwiftUI
+import UIKit
 
 struct EssenceDescriptionViewModel: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
 struct EssenceDescriptionViewModel_Previews: PreviewProvider {
     static var previews: some View {
         EssenceDescriptionViewModel()
